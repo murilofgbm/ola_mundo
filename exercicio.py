e807a24925752a65bd7899b7b810881d018ac65f
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-03-06 22:22:26 +0000

    exercicio

---
+++ exercicio.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+endereco = input("Digite seu endereco: ")
+profissao = input("Digite sua profissao: ")
+
+print(f"Nome: {nome} - Endereço: {endereco} - Profissao: {profissao}")
