523ccc0482c6ce3c742bc25908752766fdd22ef8
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-03-08 23:57:25 +0000

    ex02

--- exercicios/ex02.py
+++ exercicios/ex02.py
@@ -1,3 +1,3 @@
 a = int( input("digite um numero: ") )  #covertendo o texto em inteiro
-b = int( input("digite um numero para ser somado ao anterior: ") )
-print(a+b)
+b = int( input("digite um numero: ") )
+print(f"O resultado da soma de {a} + {b} = {a+b}")
