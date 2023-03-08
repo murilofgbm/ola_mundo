30e710b1ee36c8c59d741b26cad22c2c5265a12e
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-03-08 23:45:39 +0000

    removido

--- exercicio.py
+++
@@ -1,5 +0,0 @@
-nome = input("Digite seu nome: ")
-endereco = input("Digite seu endereco: ")
-profissao = input("Digite sua profissao: ")
-
-print(f"Nome: {nome} - Endereço: {endereco} - Profissao: {profissao}")
