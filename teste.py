88445d54869b6c1e0bc3425bf2d047c060119474
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-03-08 23:08:21 +0000

    teste

--- teste.py
+++ teste.py
@@ -3,4 +3,4 @@ print("Olá " + variavel_nome)
 
 variavel_idade = input("Escreva a idade: ")
 print (f"O(a) {variavel_nome} possui {variavel_idade} anos.") #o F abri espaco para inserir
-# (OUTRA OPCAO)print ("O(a) " + variavel_nome + " tem " + variavel_idade + " anos")
+# (OUTRA OPCAO)print ("O(a) " + variavel_nome + " tem " + variavel_idade + " anos") 
