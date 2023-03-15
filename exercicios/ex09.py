393e22a25d6b2949bb55eed511fd51cab1921ea4
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-03-15 23:39:26 +0000

    exercicios

---
+++ exercicios/ex09.py
@@ -0,0 +1,7 @@
+a = int(input("Digite um número: "))
+b = int(input("Digite um número: "))
+
+if a>b:
+    print(a)
+else:
+    print(b)
---
+++ exercicios/ex10.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um número: "))
+
+if numero > 0:
+    print("POSITIVO")
+if numero < 0:
+    print("NEGATIVO") 
+if numero == 0:
+    print("ZERO") 
