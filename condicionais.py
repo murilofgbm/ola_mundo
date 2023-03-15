84cb717c0832066fe3b3c1c908d82bf483a57906
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-03-15 23:30:09 +0000

    condicionais

---
+++ condicionais.py
@@ -0,0 +1,15 @@
+numero = int(input("Digite um número: "))
+
+if numero == 50:
+    print("O número é igual a 50")
+else:
+    print("O número é diferente de 50")
+
+
+nome = input("Digite seu nome: ")
+
+if nome == "murilo":
+    print("O nome é igual a murilo")
+else:
+    print("O nome é diferente de murilo")
+
