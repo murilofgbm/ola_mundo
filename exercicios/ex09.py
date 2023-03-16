a0415d8286ab64963304f44b3675cf46d9f11787
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-03-16 00:16:29 +0000

    exercicio

--- exercicios/ex09.py
+++ exercicios/ex09.py
@@ -1,7 +1,10 @@
 a = int(input("Digite um número: "))
 b = int(input("Digite um número: "))
+c = int(input("Digite um número: "))
 
-if a>b:
+if a>b and a>c:
     print(a)
-else:
+if b>a and b>c:
     print(b)
+if c>a and c>b:
+    print(c)
