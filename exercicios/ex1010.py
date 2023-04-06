2bc122092c6cb993195bcd8015ed21fab26df0af
murilo <39842604+murilofgbm@users.noreply.github.com>
2023-04-06 00:10:13 +0000

    beecrowd

---
+++ exercicios/ex1010.py
@@ -0,0 +1,12 @@
+linha1 = input().split()
+c1 = int(linha1[0])
+n1 = int(linha1[1])
+v1 = float(linha1[2])
+
+linha2 = input().split()
+c2 = int(linha2[0])
+n2 = int(linha2[1])
+v2 = float(linha2[2])
+
+resultado = n1*v1 + n2*v2
+print(f"VALOR A PAGAR: R$ {resultado:.2f}")
